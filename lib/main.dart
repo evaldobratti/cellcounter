e6b71ddf4ec8f321def3efcb799e6e2c9d40e4f5
Evaldo Bratti <evaldo.bratti@gmail.com>
2021-05-23 17:25:50 -0300

    getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:audioplayers/audio_cache.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:cellcounter/state.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:wakelock/wakelock.dart';
 
 void main() {
@@ -26,52 +27,57 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final AudioCache audio = new AudioCache();
-  AudioPlayer player;
-  int _counter = 0;
-
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter = 0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final Controller c = Get.put(Controller());
+
     return Scaffold(
+      drawer: Drawer(
+         child: ListView(
+           // Important: Remove any padding from the ListView.
+           padding: EdgeInsets.zero,
+           children: <Widget>[
+             DrawerHeader(
+               decoration: BoxDecoration(
+                 color: Colors.blue,
+               ),
+               child: Text('Cell counter'),
+             ),
+             ListTile(
+               title: Text('Historic'),
+               onTap: () {
+                 Get.back();
+                 Get.to(HistoricWidget());
+               },
+             )
+           ],
+         ),
+       ),
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Cell counter"),
       ),
       body: GestureDetector(
         behavior: HitTestBehavior.translucent,
-        onTapDown: (_) { increment(); },
+        onTapDown: (_) => c.increment(),
         child: Stack(
         children: [
            Column(
@@ -83,23 +89,63 @@ class _MyHomePageState extends State<MyHomePage> {
                  ),
              ],
            ),
-           Center(child: Text(_counter.toString(), style: Theme.of(context).primaryTextTheme.headline1,))
+           Center(
+            child: Obx(() => Text(c.count.toString(), style: Theme.of(context).primaryTextTheme.headline1,) )
+
+           )
         ],
       ))
         ,
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => c.zero(),
         tooltip: 'Increment',
         backgroundColor: Colors.red,
         child: Icon(Icons.delete),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
 
-  void increment() {
-    setState(() {
-      _counter++;
-      audio.play("beep-23.wav");
-    });
+class HistoricWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Controller c = Get.find();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cell counter"),
+      ),
+      body: ListView.separated(
+          itemBuilder: (ctx, ix) {
+            CounterAction action = c.actions[c.actions.length - 1 - ix];
+
+            return GestureDetector(
+              onTap: () {
+                c.setTo(int.parse(action.payload));
+                Get.back();
+              },
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text("${action.timestamp.day}/${action.timestamp.month}/${action.timestamp.year}"),
+                      Text("${action.timestamp.hour}:${action.timestamp.minute}:${action.timestamp.second}.${action.timestamp.millisecond}"),
+                    ],
+                  ),
+                  Text(action.name),
+                  Text(action.payload)
+
+                ],
+          ),
+              ),
+            );
+          },
+          separatorBuilder: (ctx, ix) => Divider(),
+          itemCount: c.actions.length
+      )
+    );
   }
 }
---
+++ lib/state.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:get/get.dart';
+import 'package:sqflite/sqflite.dart';
+
+class CounterAction {
+  int id;
+  String name;
+  DateTime timestamp;
+  String payload;
+
+  CounterAction(this.id, this.name, this.timestamp, this.payload);
+}
+
+class Controller extends GetxController{
+  final AudioCache audio = new AudioCache();
+  final AudioPlayer player = new AudioPlayer();
+  Future<File> beepAudio;
+  Database db;
+
+  Controller() {
+    beepAudio = audio.load("beep-23.wav");
+    recoverDatabase();
+  }
+
+  recoverDatabase() async {
+    var dbPath = await getDatabasesPath();
+    var path = "$dbPath/cellcounter.db";
+    db = await openDatabase(
+        path,
+      version: 1,
+      onCreate: (db, version) async {
+          print(version);
+        await db.execute('CREATE TABLE counter_actions (id INTEGER PRIMARY KEY, name TEXT, payload TEXT, timestamp_millis INT)');
+      }
+    );
+
+    List<Map> records = await db.rawQuery("SELECT id, name, payload, timestamp_millis FROM counter_actions");
+
+    records.forEach((element) {
+      var timestamp = DateTime.fromMillisecondsSinceEpoch(element["timestamp_millis"]);
+
+      actions.add(
+        CounterAction(element["id"], element["name"], timestamp, element["payload"])
+      );
+    });
+  }
+
+  var count = 0.obs;
+  var actions = [].obs;
+
+  increment() {
+    count++;
+    beep();
+    insert("increment", DateTime.now(), count.toString());
+  }
+
+  zero() {
+    count.value = 0;
+    insert("zero", DateTime.now(), count.toString());
+  }
+
+  void insert(String name, DateTime timestamp, String payload) async {
+    int id = await db.rawInsert(
+        "INSERT INTO counter_actions(name, timestamp_millis, payload) VALUES (?, ?, ?)",
+        [
+          name,
+          timestamp.millisecondsSinceEpoch,
+          payload
+        ]
+    );
+
+    actions.add(CounterAction(id, name, timestamp, payload));
+  }
+
+  setTo(int value) {
+    count.value = value;
+    insert("changed", DateTime.now(), value.toString());
+  }
+
+  beep() async {
+    player.play((await beepAudio).path);
+  }
+}
